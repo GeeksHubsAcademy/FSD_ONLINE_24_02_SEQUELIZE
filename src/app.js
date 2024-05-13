dffa4d6036db49234d2a72d2e5bc430f1d41bb88
datata <dani@danitarazona.dev>
2024-05-13 19:51:33 +0200

    feat: healthy endpoint

--- src/app.js
+++ src/app.js
@@ -7,6 +7,15 @@ const app = express()
 
 const PORT = process.env.PORT || 4000
 
+app.get('/api/healthy', (req, res) => {
+  res.status(200).json(
+    {
+      success: true,
+      message: "My APP server is healthy" 
+    }
+  )
+})
+
 app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`);
 })
